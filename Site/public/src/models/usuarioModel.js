acae4ddac9e692dcfb212a4b7d4bb81684559630
Walderley De Oliveira Santos Neto <woneto@stefanini.com>
2022-04-04 21:08:40 -0300

    aplicação da api 1

---
+++ Site/public/src/models/usuarioModel.js
@@ -0,0 +1,36 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n"+instrucao);
+    return database.executar(instrucao);
+    
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n"+instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar(nome, email, senha, CNPJ, contato, num_endereco, cep, qtd_vagas) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, CNPJ, contato, num_endereco, cep, qtd_vagas);
+    
+    var instrucao = `
+        INSERT INTO usuario (CNPJ, nome, email, contato, cep, num_endereco, senha, qtdVagas) VALUES ('${CNPJ}', '${nome}', '${email}', '${contato}', '${cep}', '${num_endereco}', '${senha}', ${qtd_vagas});
+    `;
+    console.log("Executando a instrução SQL: \n"+instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+};
