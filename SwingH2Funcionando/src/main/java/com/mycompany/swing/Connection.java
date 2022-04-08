12df531f2a5808759a1005e1f4b1b75bc81130f4
Bruno Henrique <brunoo_henriquee_chagas@hotmail.com>
2022-04-08 15:24:00 -0300

    SWING CONECTADO NO BANCO LOCAL MYSQL

--- SwingH2Funcionando/src/main/java/com/mycompany/swing/Connection.java
+++ SwingH2Funcionando/src/main/java/com/mycompany/swing/Connection.java
@@ -10,23 +10,23 @@ public class Connection {
     public Connection() {
         this.datasource = new BasicDataSource();
         
-        this.datasource.setDriverClassName("org.h2.Driver");
-//para MySQL  this.datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+       // this.datasource.setDriverClassName("org.h2.Driver");
+     this.datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
        
-        this.datasource.setUrl("jdbc:h2:file:/home/aluno/.m2/repository/com/h2database/h2/1.4.199/meu_banco");
-//Para MySQL  this.datasource.setUrl("jdbc:mysql://localhost:3360/meu_banco"); 
+      //  this.datasource.setUrl("jdbc:h2:file:/home/aluno/.m2/repository/com/h2database/h2/1.4.199/meu_banco");
+      this.datasource.setUrl("jdbc:mysql://localhost/KeepSwimming?useTimezone=true&serverTimezone=UTC"); 
 
          // usuário padrao H2
-        this.datasource.setUsername("sa");
+      //  this.datasource.setUsername("sa");
         
          // usuário utilizado em um container MySQL
-        //this.datasource.setUsername("root");
+        this.datasource.setUsername("root");
 
         // senha padrao H2
-        this.datasource.setPassword("");
+      //  this.datasource.setPassword("");
         
          // senha padrao utilizada no container MySQL
-        //this.datasource.setPassword("teste");
+        this.datasource.setPassword("YES");
     }
     
      public BasicDataSource getDatasource() {
