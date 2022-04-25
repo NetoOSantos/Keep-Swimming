cc216b518024cf438b856105ca48ee834e39ca0c
veniti-oficial <victor.smelo@bandtec.com.br>
2022-04-25 13:20:51 -0300

    style - tela login-cadastro

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -25,8 +25,8 @@ function listar(req, res) {
 }
 
 function entrar (req, res) {
-    var email = req.body.email;
-    var senha = req.body.senha;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
