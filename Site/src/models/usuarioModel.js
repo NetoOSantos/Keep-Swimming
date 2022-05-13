5e7801616b513751ea8eac5d75b77895cfefed9c
Lucas Alves <lucas.alves@valemobi.com.br>
2022-05-13 02:27:36 -0300

    ZERANDO HAMBEINETES

--- Site/src/models/usuarioModel.js
+++ Site/src/models/usuarioModel.js
@@ -6,7 +6,7 @@ var database = require("../database/config")
 // todas as funçoes abaixo fazem uma requisiçao no banco em formato de QUARY e retornam a resposta obitida
 // Tendo os dois formatos mais utilizados para teste 
 // Sendo MySQL da forma local e Azure com SQLServer de forma remota(nuvem)
-//  SEMPREEE que for usar uma das formas NAOO ESQUEÇA de comentar a outra
+//  SEMPREEEEE que for usar uma das formas NAOO ESQUEÇA de comentar a outra
 
 function listar() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
