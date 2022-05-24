89bad62f431fb3306c178dbd40416095c4c25b70
Walderley De Oliveira Santos Neto <woneto@stefanini.com>
2022-05-24 20:36:27 -0300

    conecao com banco

--- JAR-inicial/src/main/java/com/mycompany/swing/Connection.java
+++ JAR-inicial/src/main/java/com/mycompany/swing/Connection.java
@@ -15,7 +15,7 @@ public class Connection {
      //this.datasource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
        
       //  this.datasource.setUrl("jdbc:h2:file:/home/aluno/.m2/repository/com/h2database/h2/1.4.199/meu_banco");
-      this.datasource.setUrl("jdbc:mysql://localhost/KeepSwimming?useTimezone=true&serverTimezone=UTC"); 
+      this.datasource.setUrl("jdbc:mysql://localhost:3306/KeepSwimming?useTimezone=true&serverTimezone=UTC"); 
     this.datasource.setUrl("jdbc:sqlserver://keepswimming.database.windows.net:1433;database=KeepSwimming;user=KeepSwimmin@keepswimming;password=2ads@grupo8;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
     
          // usuário padrao H2
